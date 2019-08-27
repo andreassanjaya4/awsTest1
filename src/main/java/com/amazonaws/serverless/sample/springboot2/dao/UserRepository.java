397586290ffb6b001399292987424249b05decfa
andreas.sanjaya4 <andreas.sanjaya4@gmail.com>
2019-08-27 11:56:28 +0700

    test

--- src/main/java/com/amazonaws/serverless/sample/springboot2/dao/UserRepository.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot2/dao/UserRepository.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Phincon on 22/08/2019.
  */
-
-public class UserRepository {//extends CrudRepository<User, String> {
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
 
