c5ce5e6f2c5e7baeadba5ae72d995e1c57b58508
andreas.sanjaya4 <andreas.sanjaya4@gmail.com>
2019-08-27 11:44:47 +0700

    test

--- src/main/java/com/amazonaws/serverless/sample/springboot2/config/SpringConfig.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot2/config/SpringConfig.java
@@ -3,12 +3,17 @@ package com.amazonaws.serverless.sample.springboot2.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Created by Phincon on 22/08/2019.
  */
 @Configuration
 @SpringBootApplication
+@EnableJpaRepositories("com.amazonaws.serverless.sample.springboot2.dao")
 @ComponentScan("com.amazonaws.serverless.sample.springboot2")
 public class SpringConfig {
+    public SpringConfig() {
+        System.out.println("construct spring");
+    }
 }
--- src/main/java/com/amazonaws/serverless/sample/springboot2/dao/UserRepository.java
+++ src/main/java/com/amazonaws/serverless/sample/springboot2/dao/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Phincon on 22/08/2019.
  */
+
 public interface UserRepository extends CrudRepository<User, String> {
 
 }
